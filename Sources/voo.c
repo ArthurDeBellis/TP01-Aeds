eb82bfb9c3b1f08fb03b600174967884efb2dc5c
PabloVizard <you@example.com>
2018-09-14 15:26:04 -0300

    corrigido o erro ao retornar as funções char

--- Sources/voo.c
+++ Sources/voo.c
@@ -17,6 +17,17 @@ void Inicia(TVoo *Voo)
   Voo -> identificadorPista = 0;
 
 }
+//Função Imprimir
+/*void ImprimirVoo(TVoo Voo)
+{
+  printf("Vid: %d\n", Voo.vid);
+  printf("Horario de Decolagem: %d:%d\n", Voo.horaDecolagem,Voo.minutosDecolagem);
+  printf("Horario de Pouso: %d:%d\n", Voo.horaPouso,Voo.minutosPouso);
+  printf("Aeroporto de Decolagem: %s\n", Voo.aeroportoDecolagem);
+  printf("Aeroporto de Pouso: %s\n", Voo.aeroportoPouso);
+  printf("Identificador de Pista: %d\n", Voo.identificadorPista);
+
+}*/
 
 /*--------------------------------------------------------------------------------------*/
                                     //Funções Get
@@ -31,43 +42,47 @@ int GetVid(TVoo Voo)
 //Função para ler a hora da decolagem
 int GetHrDecolagem(TVoo Voo)
 {
-  return Voo.horaDecolagem;
+  return (Voo.horaDecolagem);
 }
 
 //Função para ler os minutos da decolagem
 int GetMinutosDecolagem(TVoo Voo)
 {
-  return Voo.minutosDecolagem;
+  return (Voo.minutosDecolagem);
 }
 
 //Função para ler a hora do pouso
 int GetHoraPouso(TVoo Voo)
 {
-  return Voo.horaPouso);
+  return (Voo.horaPouso);
 }
 
 //Função para ler o vid
 int GetMinutosPouso(TVoo Voo)
 {
-  return Voo.minutosPouso);
+  return (Voo.minutosPouso);
 }
 
 //Função para ler o aeroporto de decolagem
 char* GetAeroportoDecolagem(TVoo Voo)
 {
-  return Voo.aeroportoDecolagem);
+  char* p;
+  p = Voo.aeroportoDecolagem;
+  return p;
 }
 
 //Função para ler o aeroporto de pouso
 char* GetAeroportoPouso(TVoo Voo)
 {
-  return Voo.aeroportoPouso);
+  char* p;
+  p = Voo.aeroportoPouso;
+  return p;
 }
 
 //Função para ler o identificador da pista
 int GetIdPista(TVoo Voo)
 {
-  return Voo.identificadorPista);
+  return (Voo.identificadorPista);
 }
 
 /*--------------------------------------------------------------------------------------*/
@@ -94,7 +109,7 @@ void SetMinutosDecolagem(TVoo *Voo)
 }
 
 //Função para alterar a hora de pouso
-void SetHoraPouso(TVoo *Voo)
+void SetHrPouso(TVoo *Voo)
 {
   scanf("%d", &Voo -> horaPouso);
 }

e34107c9106ed567aa9eceecd09325122c6cc563
arthur <arthur.bellis@ufv.br>
2018-09-15 00:31:19 -0300

    Estruturação básica do ItemMatriz.h

---
+++ Libs/ItemMatriz.h
@@ -0,0 +1,32 @@
+#include "../Libs/voo.h"
+#include "../Libs/ListadeVoos.h"
+#ifndef ITEMMATRIZ_H
+#define ITEMMATRIZ_H
+
+//Definido a struct
+typedef struct {
+  //Lista do tipo TLista, definido no TAD Lista_de_Voos
+  TLista Lista;
+  //Numero total de voos na lista
+  int NumeroVoos;
+  //Hora da ultima atualização
+  int HrLast;
+  //Minutos da ultima atualização
+  int MntsLast;
+}ItemMatriz;
+
+void iniciar (ItemMatriz *Item);
+
+/*Funções SET*/
+
+void SetLista(ItemMatriz *Item);
+void SetNumVoo(ItemMatriz *Item);
+void SetHrLast(ItemMatriz *Item);
+void SetMntsLast(ItemMatriz *Item);
+
+/*Funções GET*/
+
+TLista GetLista(ItemMatriz Item);
+int GetNumVoo(ItemMatriz Item);
+int GetHrLast(ItemMatriz Item);
+int GetMntsLast(ItemMatriz Item);
